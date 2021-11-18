02220b9c63d30d32e3e3e066992d7e559b876814
Andrew Cheung <cheungdzinyung@gmail.com>
2021-11-18 12:22:30 -0500

    renamed files

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -1,13 +1,13 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { AddressBookPage, DetailPage, NotFound } from "../pages";
+import { AddressBookPage, ContactDetailsPage, NotFoundPage } from "../pages";
 
 export const Router = () => (
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<AddressBookPage />} />
-      <Route path="/contact/:uuid" element={<DetailPage />} />
-      <Route path="*" element={<NotFound />} />
+      <Route path="/contact/:uuid" element={<ContactDetailsPage />} />
+      <Route path="*" element={<NotFoundPage />} />
     </Routes>
   </BrowserRouter>
 );
