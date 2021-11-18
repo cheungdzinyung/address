e4861373370e148982dad1334096397b092a5ef0
Andrew Cheung <cheungdzinyung@gmail.com>
2021-11-17 19:53:32 -0500

    Added linter and package updates

--- src/App.js
+++ src/App.js
@@ -1,22 +1,11 @@
 import React from "react";
-import Router from "./routes/Routes";
-import { useFetchAllUsers } from "./api/Request";
+import { Router } from "./routes";
 import "./App.css";
-import loading from "./assets/images/loading.gif";
 
 function App() {
-  const { users, isLoading, hasError } = useFetchAllUsers();
-
   return (
     <div className="App">
-      {hasError && <p>Something went wrong</p>}
-      {isLoading ? (
-        <div id="spinner">
-          <img src={loading} alt="loading" />
-        </div>
-      ) : (
-        <Router users={users} />
-      )}
+      <Router />
     </div>
   );
 }
