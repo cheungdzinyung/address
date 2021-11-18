0d6187533f6202a7742fc7b27e4a23b1a0046bc3
Andrew Cheung <cheungdzinyung@gmail.com>
2021-11-18 12:20:21 -0500

    refactor fetch

--- src/pages/AddressBookPage.js
+++ src/pages/AddressBookPage.js
@@ -1,13 +1,18 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import { AddressBook } from "../components";
-import { useFetch } from "../hooks/UseFetch";
+import { useFetch, useAddressBook } from "../hooks";
 import loading from "../assets/images/loading.gif";
 
 export const AddressBookPage = () => {
-  const url =
-    "https://randomuser.me/api/?results=50&nat=us,ca,gb&inc=name,picture,login,phone,cell&seed=nuvalence";
-  const { data, isLoading, hasError } = useFetch(url);
-  const [contacts, setContacts] = useState([]);
+  const { contacts, setContacts } = useAddressBook();
+
+  const { data, isLoading, hasError } = useFetch(
+    "https://randomuser.me/api/?results=50&nat=us,ca,gb&inc=name,picture,login,phone,cell&seed=nuvalence",
+    // for simplicity, use contacts data to determine whether the data need to be refetched or not, 
+    // should use page no or other parameter to check.
+    // if have more pages, should use page number to determine if refetch is needed
+    !contacts || contacts.length === 0 
+  );
 
   useEffect(() => {
     if (data?.results) {
@@ -17,7 +22,7 @@ export const AddressBookPage = () => {
       }
       setContacts(fetchedContacts);
     }
-  }, [data]);
+  }, [data, setContacts]);
 
   return (
     <div>
@@ -27,7 +32,7 @@ export const AddressBookPage = () => {
           <img src={loading} alt="loading" />
         </div>
       )}
-      {contacts.length > 0 && <AddressBook contacts={contacts} />}
+      {contacts && contacts.length > 0 && <AddressBook contacts={contacts} />}
     </div>
   );
 };
