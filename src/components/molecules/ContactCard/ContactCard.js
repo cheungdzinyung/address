c1ec7a46f3debbbffd1d2fabebde08ada0d24aff
Andrew Cheung <cheungdzinyung@gmail.com>
2021-11-17 23:43:33 -0500

    updated ContactCard Styling

--- src/components/molecules/ContactCard/ContactCard.js
+++ src/components/molecules/ContactCard/ContactCard.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import styles from "./ContactCard.module.css";
 
 export const ContactCard = ({ contact }) => (
-  <div>
+  <div className={styles.contactCard}>
     <Link to={`contact/${contact.login.uuid}`} state={contact}>
-      <img src={contact.picture.thumbnail} alt="avatar" />
+      <img src={contact.picture.medium} alt="avatar" />
       {contact.name.title}. {contact.name.first} {contact.name.last}
     </Link>
   </div>
