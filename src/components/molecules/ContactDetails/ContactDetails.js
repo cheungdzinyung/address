37bffd4d7e423f4487e09a18bb77b677a2211a91
Andrew Cheung <cheungdzinyung@gmail.com>
2021-11-17 23:43:50 -0500

    updated ContactDetail styling

--- src/components/molecules/ContactDetails/ContactDetails.js
+++ src/components/molecules/ContactDetails/ContactDetails.js
@@ -1,20 +1,21 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
+import styles from "./ContactDetails.module.css";
 
 export const ContactDetails = () => {
   const location = useLocation();
   const contact = location.state;
 
   return (
-    <div>
+    <div className={styles.contactDetails}>
       {contact && (
         <>
           <img src={contact.picture.large} alt="profile" />
-          <h1>
+          <h2>
             {contact.name.title}. {contact.name.first} {contact.name.last}
-          </h1>
-          <h2>Phone: {contact.phone} </h2>
-          <h2>Cell: {contact.cell}</h2>
+          </h2>
+          <h3>Phone: {contact.phone} </h3>
+          <h3>Cell: {contact.cell}</h3>
         </>
       )}
     </div>
