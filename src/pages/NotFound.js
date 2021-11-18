e4b8cd480265ce19f8d8318928be0181efbfba9f
Andrew Cheung <cheungdzinyung@gmail.com>
2021-11-18 12:19:49 -0500

    remaned files

--- src/pages/NotFound.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { Link } from "react-router-dom";
-
-export const NotFound = () => (
-  <div>
-    <h1>Oops...nothing here</h1>
-    <Link to="/">Back to Home</Link>
-  </div>
-);
