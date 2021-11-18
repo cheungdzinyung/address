da9d9c15b921f5383be9a184ffe8bcd46ef2f2f5
Andrew Cheung <cheungdzinyung@gmail.com>
2021-11-17 23:42:32 -0500

    Added header with title and image

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { Router } from "./routes";
 import "./App.css";
+import { Header } from "./components/atoms/Header/Header";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Router />
     </div>
   );
---
+++ src/components/atoms/Header/Header.js
@@ -0,0 +1,9 @@
+import React from "react";
+import logo from "../../../assets/images/address-book-thumbnail.png";
+
+export const Header = () => (
+  <div>
+    <img src={logo} alt="Address Book Logo" />
+    <h1> Address Book </h1>
+  </div>
+);
